3cd61c2dffa4a361ea04463dee81aafc22eff48d
mohit123-g <137385621+mohit123-g@users.noreply.github.com>
2023-06-27 03:17:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,37 +5,60 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import { render } from "@testing-library/react";
 export default function App() {
-  
-  const [songs, setSongs] = useState([
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/A.mp3", name: "Aaaram" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/B.mp3", name: "Doremon" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/C.mp3", name: "Dil duba" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/B.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/D.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/C.mp3"},
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/E.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/F.mp3"},
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/G.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/H.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/I.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/J.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/K.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/L.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/M.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/N.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/O.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/P.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/Q.mp3"},
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/R.mp3"},
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/S.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/T.mp3"  },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/U.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/V.mp3"  },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/W.mp3" },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/X.mp3"  },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/Y.mp3"  },
-    { audio: "http://192.168.0.103:4444/api/fs/storage/emulated/0/Ringtones/Z.mp3"  },
+  import song1 from "./songs/A.mp3";
+  import song2 from "./songs/B.mp3";
+  import song3 from "./songs/C.mp3";
+  import song4 from "./songs/D.mp3";
+  import song5 from "./songs/E.mp3";
+  import song6 from "./songs/F.mp3";
+  import song7 from "./songs/G.mp3";
+  import song8 from "./songs/H.mp3";
+  import song9 from "./songs/I.mp3";
+  import song10 from "./songs/J.mp3";
+  import song11 from "./songs/K.mp3";
+  import song12 from "./songs/L.mp3";
+  import song13 from "./songs/M.mp3";
+  import song14 from "./songs/N.mp3";
+  import song15 from "./songs/O.mp3";
+  import song16 from "./songs/P.mp3";
+  import song17 from "./songs/Q.mp3";
+  import song18 from "./songs/R.mp3";
+  import song19 from "./songs/S.mp3";
+  import song20 from "./songs/T.mp3";
+  import song21 from "./songs/U.mp3";
+  import song22 from "./songs/V.mp3";
+  import song23 from "./songs/W.mp3";
+  import song24 from "./songs/X.mp3";
+  import song25 from "./songs/Y.mp3";
+  import song26 from "./songs/Z.mp3";
 
+  const [songs, setSongs] = useState([
+    { audio:song1 , name: "" },
+    { audio:song2 , name: "" },
+    { audio:song3 , name: "" },
+    { audio:song4 , name: "" },
+    { audio:song5 , name: "" },
+    { audio:song6 , name: "" },
+    { audio:song7 , name: "" },
+    { audio:song8 , name: "" },
+    { audio:song9 , name: "" },
+    { audio:song10 , name: "" },
+    { audio:song11, name: "" },
+    { audio:song12 , name: "" },
+    { audio:song13 , name: "" },
+    { audio:song14 , name: "" },
+    { audio:song15 , name: "" },
+    { audio:song16 , name: "" },
+    { audio:song17 , name: "" },
+    { audio:song18 , name: "" },
+    { audio:song19 , name: "" },
+    { audio:song20 , name: "" },
+    { audio:song21 , name: "" },
+    { audio:song22 , name: "" },
+    { audio:song23 , name: "" },
+    { audio:song24 , name: "" },
+    { audio:song25 , name: "" },
+    { audio:song26 , name: "" }
   ]);
   const [search, setSearch] = useState("");
   return (
